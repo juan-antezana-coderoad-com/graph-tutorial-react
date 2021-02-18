1962ca16377e1a486f49dcf067faffc0e3cf998a
Juan Antezana Adrian <juan.antezana@mojix.com>
2021-02-18 15:56:04 -0400

    Initial implementation

---
+++ src/Config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    appId: '4d251083-a765-4d23-99e7-ffa500ec75bd',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+        'user.read',
+        'mailboxsettings.read',
+        'calendars.readwrite'
+    ]
+};
